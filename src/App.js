21d3e60eaf35dbba86353cb9dca9f1b2c3285d3c
Robert Pliszka <robplisz@gmail.com>
2023-08-22 18:19:24 -0400

    Added UserList to be displayed in the app component which currently has no data.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import AddUser from "./Components/AddUser/AddUser";
+import UserList from "./Components/UserList/UserList";
 
 function App() {
   return (
     <div>
       <AddUser />
+      <UserList users={[]}/>
     </div>
   );
 }
