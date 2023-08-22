a4b0ec33d692d7af12590a10abd63e453767caa4
Robert Pliszka <robplisz@gmail.com>
2023-08-22 16:54:19 -0400

    Created a function to handle each input and have the form function print out the values which are stored in state

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -1,19 +1,41 @@
+import React, { useState } from "react";
 import styles from "./AddUser.module.css";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
 
 function AddUser() {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
   const addUserHandler = (event) => {
     event.preventDefault();
+
+    console.log(enteredUsername, enteredAge);
+
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageHandler = (event) => {
+    setEnteredAge(event.target.value);
   };
 
   return (
     <Card className={styles.input}>
       <form className={styles.input} onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
-        <input id="username" type="text"></input>
+        <input
+          id="username"
+          type="text"
+          onChange={usernameHandler}
+          value={enteredUsername}
+        ></input>
         <label htmlFor="user-age">Age (Years)</label>
-        <input id="user-age" type="number"></input>
+        <input id="user-age" type="number" onChange={ageHandler} value={enteredAge}></input>
         <Button type="submit">Add User</Button>
       </form>
     </Card>
