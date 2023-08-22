bb3eb2fc2a70b3491c1c3b273971de92ef3623de
Robert Pliszka <robplisz@gmail.com>
2023-08-22 18:14:04 -0400

    Created the userList component to display users information

--- src/Components/UserList/UserList.js
+++ src/Components/UserList/UserList.js
@@ -1,5 +1,18 @@
 import styles from "./UserList.module.css";
+import Card from "../Card/Card";
 
-function UserList() {}
+function UserList(props) {
+  return (
+    <Card className={styles.users}>
+      <ul>
+        {props.users.map((user) => {
+          <li>
+            {user.name} ({user.age}) years old.
+          </li>;
+        })}
+      </ul>
+    </Card>
+  );
+}
 
 export default UserList;
