c0b52070f7c804496d8bc2648126ce4617a198de
Robert Pliszka <robplisz@gmail.com>
2023-08-23 17:52:44 -0400

    Created the modal to display error messages when user input not correct

--- src/Components/AddUser/AddUser.js
+++ src/Components/AddUser/AddUser.js
@@ -2,18 +2,28 @@ import React, { useState } from "react";
 import styles from "./AddUser.module.css";
 import Card from "../Card/Card";
 import Button from "../Button/Button";
+import ErrolModal from "../ErrorModal/ErrorModal";
 
 function AddUser(props) {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "Invlaid input",
+        message: "Please enter a valid name and age (non-empty values).",
+      });
       return;
     }
     if (+enteredAge < 1) {
+      setError({
+        title: "Invalid age",
+        message: "Please enter a valid age (>0)",
+      });
       return;
     }
 
@@ -30,26 +40,39 @@ function AddUser(props) {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
+
   return (
-    <Card className={styles.input}>
-      <form className={styles.input} onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          onChange={usernameHandler}
-          value={enteredUsername}
-        ></input>
-        <label htmlFor="user-age">Age (Years)</label>
-        <input
-          id="user-age"
-          type="number"
-          onChange={ageHandler}
-          value={enteredAge}
-        ></input>
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+    <div>
+      {error && (
+        <ErrolModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
+      <Card className={styles.input}>
+        <form className={styles.input} onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            onChange={usernameHandler}
+            value={enteredUsername}
+          ></input>
+          <label htmlFor="user-age">Age (Years)</label>
+          <input
+            id="user-age"
+            type="number"
+            onChange={ageHandler}
+            value={enteredAge}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 }
 
--- src/Components/Button/Button.js
+++ src/Components/Button/Button.js
@@ -2,7 +2,11 @@ import styles from "./Button.module.css";
 
 function Button(props) {
   return (
-    <button className={styles.button} type={props.type || "button"}>
+    <button
+      className={styles.button}
+      type={props.type || "button"}
+      onClick={props.onClick}
+    >
       {props.children}
     </button>
   );
